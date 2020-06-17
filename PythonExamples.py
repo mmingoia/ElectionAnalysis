0e180fd74a4e128d35f4c094c77b64f23d74a707
mmingoia <michaelmingoia77@gmail.com>
2020-06-16 21:38:37 -0500

    Adding the election analysis

---
+++ PythonExamples.py
@@ -0,0 +1,19 @@
+# Variables
+
+exampleString = "Hello World"
+exampleString2 = 'Hello World'
+exampleString3 = "Hello, Mike"
+exampleString4 = 'Hello, Mike'
+
+#print(exampleString)
+#print(type(exampleString))
+
+myName = "Mike Mingoia"
+myAge = 42
+myBD = "8/13/1977"
+
+myIntroduction = f"""
+Hello, my name is {myName} \nand I am {myAge}\nand I was born on {myBD}
+"""
+
+print(myIntroduction)
